c5235f889bc3a3c1cb16f8f8717772c9e430de48
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2024-02-21 03:40:19 +0000

    Add files via upload

---
+++ pypiserver/README.md
@@ -0,0 +1,3 @@
+# Python Server
+
+This repository defines a very simple package that just has a function that returns "world".
---
+++ pypiserver/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_world",
+    version="0.0.1",
+    author="Moses Fabiyi",
+    author_email="moses.f@igelimited.com",
+    description="A function that returns 'world'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ecampuslearning/python_world",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
